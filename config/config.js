b749318beb224301dab3f1e37d23c6f8a9c6ee90
Juan Lorenzo Sáez <juanlorenzosaez@MacBook-Pro-de-Juan.local>
2021-11-16 18:53:18 +0100

    initial commit

---
+++ config/config.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+// Deploying in an non-ssl environment:
+// npx sequelize-cli db:migrate
+
+// Deploying in an ssl environment
+// npx sequelize-cli db:migrate --env development-ssl
+
+module.exports = {
+    development: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 3306,
+        dialect: 'mysql',
+        logging: true,
+    },
+    test: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 3306,
+        dialect: 'mysql',
+    },
+    production: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 3306,
+        dialect: 'mysql',
+    },
+};
---
+++ index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const path = require('path');
+
+const express = require('express');
+
+require('dotenv').config({ path: path.join(__dirname, '.env') });
+
+const routes = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+app.use('/', routes);
+
+module.exports = app;
