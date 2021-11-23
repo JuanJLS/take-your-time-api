21136052e2f2ca2728afd663091cdefcbc2931b5
Juan Lorenzo Sáez <juanlorenzosaez@gmail.com>
2021-11-23 20:14:14 +0100

    Added Project model

---
+++ models/project.js
@@ -0,0 +1,43 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class Project extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (task) {
+      // define association here
+      // Project.hasMany(task, { primaryKey: 'id' })
+    }
+  };
+  Project.init({
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    }
+  }, {
+    sequelize,
+    modelName: 'Project',
+    tableName: 'Projects',
+    timestamp: true
+  })
+  return Project
+}
